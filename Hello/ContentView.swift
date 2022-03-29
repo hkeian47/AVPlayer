307ef8a2b9ca115e6fb0219e731f88f48501993b
Hana <hanakeinan11@gmail.com>
2022-03-29 12:40:42 -0500

    AVPlayer

--- Hello/ContentView.swift
+++ Hello/ContentView.swift
@@ -6,14 +6,21 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State var player = AVPlayer()
+    var videoURL = URL(string: "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4")
+                       
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+        VideoPlayer(player: player)
+            .ignoresSafeArea()
+            .onAppear{
+                player = AVPlayer(url:videoURL!)
+            }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
