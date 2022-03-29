1af76ac583135432b727baf4d4c16f2228cd80b1
Hana <hanakeinan11@gmail.com>
2022-03-29 12:26:35 -0500

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Hana Keinan on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
